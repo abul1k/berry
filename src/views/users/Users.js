e7ce7ad5743011a8deb525a15bb531fa68351dad
zkpc404 <zkpc@gmail.com>
2023-03-01 18:21:38 +0500

    user modal

--- src/views/users/Users.js
+++ src/views/users/Users.js
@@ -1,5 +1,7 @@
 // material-ui
 
+import React, { useState } from 'react';
+
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import IconButton from '@mui/material/IconButton';
@@ -11,6 +13,7 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import UserModal from './components/UserModal';
 
 function createData(name, calories, fat, carbs, protein) {
     return { name, calories, fat, carbs, protein };
@@ -28,44 +31,71 @@ const rows = [
 
 // ==============================|| USERS PAGE ||============================== //
 
-const Users = () => (
-    <MainCard title="Users Card">
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">Calories</TableCell>
-                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                        <TableCell align="right">Actions</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                            <TableCell component="th" scope="row">
-                                {row.name}
-                            </TableCell>
-                            <TableCell align="right">{row.calories}</TableCell>
-                            <TableCell align="right">{row.fat}</TableCell>
-                            <TableCell align="right">{row.carbs}</TableCell>
-                            <TableCell align="right">{row.protein}</TableCell>
-                            <TableCell align="right">
-                                <IconButton color="warning" size="small">
-                                    <EditIcon />
-                                </IconButton>
-                                <IconButton color="error" size="small">
-                                    <DeleteIcon />
-                                </IconButton>
-                            </TableCell>
+const Users = () => {
+    let [user, setUser] = useState({
+        name: '',
+        calories: null,
+        fat: null,
+        carbs: null,
+        protein: null
+    });
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = (data) => {
+        setOpen(true);
+        setUser(
+            (user = {
+                name: data.name,
+                calories: data.calories,
+                fat: data.fat,
+                carbs: data.carbs,
+                protein: data.protein
+            })
+        );
+        console.log(user);
+    };
+    const handleClose = () => setOpen(false);
+
+    return (
+        <MainCard title="Users Card">
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Dessert (100g serving)</TableCell>
+                            <TableCell align="right">Calories</TableCell>
+                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                            <TableCell align="right">Actions</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    </MainCard>
-);
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align="right">{row.calories}</TableCell>
+                                <TableCell align="right">{row.fat}</TableCell>
+                                <TableCell align="right">{row.carbs}</TableCell>
+                                <TableCell align="right">{row.protein}</TableCell>
+                                <TableCell align="right">
+                                    <IconButton onClick={() => handleOpen(row)} color="warning" size="small">
+                                        <EditIcon />
+                                    </IconButton>
+                                    <IconButton color="error" size="small">
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <UserModal isOpen={open} handleClose={handleClose} user={user} />
+        </MainCard>
+    );
+};
 
 export default Users;
